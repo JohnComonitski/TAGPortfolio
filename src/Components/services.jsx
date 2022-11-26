be733c6f521788c595ffe845ec3dc9d6ca0533e6
JohnComonitski <johncomonitski@gmail.com>
2022-11-26 16:31:12 -0500

    stats

--- src/Components/services.jsx
+++ src/Components/services.jsx
@@ -67,10 +67,10 @@ export default function Services() {
             <div className="counter-area mt-45">
                 <div className="container">
                     <div className="row">
-                        <SingleCountdown extraClass="mb-4 mb-md-0" title="Followers" start={0} end={9032} duration={2}/>
-                        <SingleCountdown extraClass="mb-4 mb-md-0" title="Totals Likes" start={0} end={333000} duration={2}/>
-                        <SingleCountdown extraClass="mb-4 mb-md-0" title="Views This Week" start={0} end={573000} duration={2}/>
-                        <SingleCountdown title="Views This Month" start={0} end={1700000} duration={2}/>
+                        <SingleCountdown extraClass="mb-4 mb-md-0" title="Followers" start={0} end={11700} duration={2}/>
+                        <SingleCountdown extraClass="mb-4 mb-md-0" title="Totals Likes" start={0} end={558100} duration={2}/>
+                        <SingleCountdown extraClass="mb-4 mb-md-0" title="Views This Week" start={0} end={757000} duration={2}/>
+                        <SingleCountdown title="Views This Month" start={0} end={2300000} duration={2}/>
                     </div>
                 </div>
             </div>
